30d9dcbad22bf002da0a47dab9f32adf6006b224
Himanshu Bhardwaj <him_bhar@hotmail.com>
2019-03-08 13:39:58 +0530

    [GH-1] Added main class for launching application, a test class for ensuring initialization and added JUnit-5 support.

---
+++ src/test/java/com/himanshu/countryblotter/LaunchTest.java
@@ -0,0 +1,15 @@
+package com.himanshu.countryblotter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LaunchTest {
+  @DisplayName("Test Spring @Autowired Integration")
+  @Test
+  void testGet() {
+    Assertions.assertTrue(true);
+  }
+}
