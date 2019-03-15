a0a0e4d632e380f5b85d1b812ff61d3347395434
Himanshu Bhardwaj <him_bhar@hotmail.com>
2019-03-15 11:49:45 +0530

    [GH-4] Added actuator endpoints also added the way to load hierarchy of properties files based on spring profiles.

--- src/main/java/com/himanshu/countryblotter/Main.java
+++ src/main/java/com/himanshu/countryblotter/Main.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+/**
+ * application-{spring-profile}.properties, it gets activated when you use -Dspring.profiles.active=<profile-name>
+ */
 @SpringBootApplication
 public class Main {
   public static void main(String[] args) {
