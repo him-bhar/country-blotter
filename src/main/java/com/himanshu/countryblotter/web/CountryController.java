88b2d39160d224117a2827ec86e20f8b5d954d0d
Himanshu Bhardwaj <him_bhar@hotmail.com>
2019-03-31 23:53:27 +0530

    [GH-8] Added swagger documentation for country controller. Visit: http://localhost:8080/swagger-ui.html after launching application.

--- src/main/java/com/himanshu/countryblotter/web/CountryController.java
+++ src/main/java/com/himanshu/countryblotter/web/CountryController.java
@@ -2,6 +2,7 @@ package com.himanshu.countryblotter.web;
 
 import com.himanshu.countryblotter.domain.Country;
 import com.himanshu.countryblotter.fetcher.ICountryFetcher;
+import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(value = "Querying countries REST API")
 @RestController
 @RequestMapping("/countries")
 public class CountryController {
@@ -23,13 +25,29 @@ public class CountryController {
   @Autowired
   private ICountryFetcher<Country> countryFetcher;
 
+  @ApiOperation(value = "List all countries", httpMethod = "GET", produces = "application/json", response = List.class
+        /*, authorizations = {
+              @Authorization(value = "basic")
+        }*/
+  )
+  @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successfully retrieved list"),
+        @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+        @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden")
+  })
   @RequestMapping(path = {"/all"}, method = {RequestMethod.GET})
   public ResponseEntity<List<Country>> getAllCountries() {
     return new ResponseEntity(countryFetcher.fetchAllCountries(), HttpStatus.OK);
   }
 
+  @ApiOperation(value = "Get a country by country-code", httpMethod = "GET", produces = "application/json", response = Country.class)
+  @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successfully retrieved country"),
+        @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+        @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden")
+  })
   @RequestMapping(path = {"/{country-code}"}, method = {RequestMethod.GET})
-  public ResponseEntity<List<Country>> getCountryByCode(@PathVariable("country-code") String countryCode) {
+  public ResponseEntity<List<Country>> getCountryByCode(@ApiParam(value = "Country code", required = true) @PathVariable("country-code") String countryCode) {
     logger.info("Looking for country: "+countryCode);
     Country country = countryFetcher.fetchCountry(countryCode);
     if (country != null) {
