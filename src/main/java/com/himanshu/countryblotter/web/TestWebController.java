29b3faa2fc537482ba278b4d382ad596024ab8ec
Himanshu Bhardwaj <him_bhar@hotmail.com>
2019-10-27 16:04:11 +0530

    Added basic web capability with a working home page.

---
+++ src/main/java/com/himanshu/countryblotter/web/TestWebController.java
@@ -0,0 +1,19 @@
+package com.himanshu.countryblotter.web;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestWebController {
+  @Value("${info.app.name}")
+  String appName;
+
+  @GetMapping("/")
+  public String homePage(Model model) {
+    model.addAttribute("appName", appName);
+    return "home";
+  }
+}
